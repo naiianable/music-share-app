4b169022a1dad08d845f12531b724f357044b1d5
Ian Yamashita <ianyamashita@Ians-Air.lan>
2022-01-13 12:03:49 -1000

    add and delete songs to queue using local storage

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -2,25 +2,28 @@ import { makeStyles } from "@mui/styles";
 import { Delete } from "@mui/icons-material";
 import { Avatar, IconButton, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_OR_REMOVE_FROM_QUEUE } from "../graphql/mutations";
 
-const QueuedSongList = () => {
+const QueuedSongList = ({ queue }) => {
+	console.log({ queue });
 	//checking to see if screen size is bigger than "md"(medium).  use greaterThanMd as a boolean to display components
 	const greaterThanMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
 
-	const song = {
-		title: "Tattoos Together",
-		artist: "Lauv",
-		thumbnail:
-			"https://i.ytimg.com/an_webp/j9DnUII28fI/mqdefault_6s.webp?du=3000&sqp=CNra044G&rs=AOn4CLBhB_Q2D-UC69iiCRWx08vBbFn28g",
-	};
+	// const song = {
+	// 	title: "Tattoos Together",
+	// 	artist: "Lauv",
+	// 	thumbnail:
+	// 		"https://i.ytimg.com/an_webp/j9DnUII28fI/mqdefault_6s.webp?du=3000&sqp=CNra044G&rs=AOn4CLBhB_Q2D-UC69iiCRWx08vBbFn28g",
+	// };
 
 	return (
 		greaterThanMd && (
 			<div style={{ margin: "10px 0" }}>
 				<Typography color="textSecondary" variant="button">
-					QUEUE (5)
+					QUEUE ({queue.length})
 				</Typography>
-				{Array.from({ length: 5 }, () => song).map((song, i) => (
+				{queue.map((song, i) => (
 					<QueuedSong key={i} song={song} />
 				))}
 			</div>
@@ -53,7 +56,24 @@ const useStyles = makeStyles({
 
 function QueuedSong({ song }) {
 	const classes = useStyles();
+	const [addOrRemoveFromQueue] = useMutation(ADD_OR_REMOVE_FROM_QUEUE, {
+		onCompleted: (data) => {
+			//returning data and setting it in local storage from client.js mutation type and resolvers
+			localStorage.setItem(
+				"queue",
+				JSON.stringify(data.addOrRemoveFromQueue)
+			);
+		},
+	});
 	const { thumbnail, artist, title } = song;
+
+	function handleAddOrRemoveFromQueue() {
+		addOrRemoveFromQueue({
+			//__typename is just the name of thing we're working with
+			//where data is provided to input in client in order to change
+			variables: { input: { ...song, __typename: "Song" } },
+		});
+	}
 
 	return (
 		<div className={classes.container}>
@@ -74,7 +94,7 @@ function QueuedSong({ song }) {
 					{artist}
 				</Typography>
 			</div>
-			<IconButton>
+			<IconButton onClick={handleAddOrRemoveFromQueue}>
 				<Delete color="error" />
 			</IconButton>
 		</div>
