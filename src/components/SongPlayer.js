27654e9c211d15718a3878fd5cb85380858dc6bf
Ian Yamashita <ianyamashita@Ians-Air.lan>
2022-01-19 11:16:49 -1000

    song player seek, next/prev song

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { makeStyles } from "@mui/styles";
 import {
 	Card,
@@ -48,10 +48,27 @@ const SongPlayer = () => {
 	//setting state for slider in player
 	const [played, setPlayed] = useState(0);
 	const [seeking, setSeeking] = useState(false);
+	const [playedSeconds, setPlayedSeconds] = useState(0);
+	const [positionInQueue, setPositionInQueue] = useState(0);
 	const { data } = useQuery(GET_QUEUED_SONGS);
 	//bringing in hard coded song from app.js. setting data to "state" and using it to update jsx in player
 	const { state, dispatch } = useContext(SongContext);
 	const classes = useStyles();
+
+	useEffect(() => {
+		const songIndex = data.queue.findIndex(
+			(song) => song.id === state.song.id
+		);
+		setPositionInQueue(songIndex);
+	}, [data.queue, state.song.id]);
+
+	useEffect(() => {
+		const nextSong = data.queue[positionInQueue + 1];
+		if (played >= 0.99 && nextSong) {
+			setPlayed(0);
+			dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+		}
+	}, [data.queue, played, dispatch, positionInQueue]);
 
 	function handleTogglePlay() {
 		dispatch(
@@ -79,6 +96,25 @@ const SongPlayer = () => {
 		reactPlayerRef.current.seekTo(played);
 	}
 
+	//formatting playedSeconds
+	function formatDuration(seconds) {
+		return new Date(seconds * 1000).toISOString().substr(11, 8);
+	}
+
+	function handlePlayNextSong() {
+		const nextSong = data.queue[positionInQueue + 1];
+		if (nextSong) {
+			dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+		}
+	}
+
+	function handlePlayPrevSong() {
+		const prevSong = data.queue[positionInQueue - 1];
+		if (prevSong) {
+			dispatch({ type: "SET_SONG", payload: { song: prevSong } });
+		}
+	}
+
 	return (
 		<>
 			<Card variant="outlined" className={classes.container}>
@@ -97,7 +133,7 @@ const SongPlayer = () => {
 					</CardContent>
 					<div className={classes.controls}>
 						<IconButton>
-							<SkipPrevious />
+							<SkipPrevious onClick={handlePlayPrevSong} />
 						</IconButton>
 						<IconButton onClick={handleTogglePlay}>
 							{state.isPlaying ? (
@@ -107,14 +143,14 @@ const SongPlayer = () => {
 							)}
 						</IconButton>
 						<IconButton>
-							<SkipNext />
+							<SkipNext onClick={handlePlayNextSong} />
 						</IconButton>
 						<Typography
 							variant="subtitle1"
 							component="p"
 							color="textSecondary"
 						>
-							00:01:30
+							{formatDuration(playedSeconds)}
 						</Typography>
 					</div>
 					<Slider
@@ -135,6 +171,7 @@ const SongPlayer = () => {
 					onProgress={({ played, playedSeconds }) => {
 						if (!seeking) {
 							setPlayed(played);
+							setPlayedSeconds(playedSeconds);
 						}
 					}}
 					url={state.song.url}
